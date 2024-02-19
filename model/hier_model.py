68aecbdbec096f92e003397cb505dfded15abc2d
moon <d.moon23k@gmail.com>
2024-02-19 20:44:21 +0900

    minor update

--- model/hier_model.py
+++ model/hier_model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from collections import namedtuple
-from .common import (
+from .components import (
     clones, Embeddings,
     PositionalEncoding, 
     PositionwiseFeedForward,
